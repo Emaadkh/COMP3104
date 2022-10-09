1c6e17d269f2314acfd0caa69fbae51ff73f70c8
Emad <emadk571@gmail.com>
2022-10-09 09:25:10 -0400

    React project code added

---
+++ build/precache-manifest.62e238ef51b8f7d8a275ce3e2f28fbf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e58a7f02d86189be2661",
+    "url": "/Emaadkh/COMP3104/static/css/main.9db2754d.chunk.css"
+  },
+  {
+    "revision": "e58a7f02d86189be2661",
+    "url": "/Emaadkh/COMP3104/static/js/main.e58a7f02.chunk.js"
+  },
+  {
+    "revision": "be2e8c7371046b6a7458",
+    "url": "/Emaadkh/COMP3104/static/js/runtime~main.be2e8c73.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Emaadkh/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "eb7c508c3be638682ddef6f01b0ca562",
+    "url": "/Emaadkh/COMP3104/index.html"
+  }
+];
